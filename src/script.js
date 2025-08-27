885d127fb0dca771e726a9ae512fb6168f02275a
Kostya-Rozh <kosta7497@gmail.com>
2025-08-27 10:20:10 +0300

    Add container styles

--- src/script.js
+++ src/script.js
@@ -1,9 +1,3 @@
 console.log('hello');
 console.log('world');
 console.log('!');
-console.log('test');
-console.log('test2');
-
-window.onload = () => {
-    alert("Здравствуйте, приветствуем вас на сайте");
-};
