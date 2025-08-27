cf89c1c729fcbd5620420131548dd3d1f85ad479
Kostya-Rozh <kosta7497@gmail.com>
2025-08-27 10:01:40 +0300

    init project

---
+++ src/script.js
@@ -0,0 +1,9 @@
+console.log('hello');
+console.log('world');
+console.log('!');
+console.log('test');
+console.log('test2');
+
+window.onload = () => {
+    alert("Здравствуйте, приветствуем вас на сайте");
+};
