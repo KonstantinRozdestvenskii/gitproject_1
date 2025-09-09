0a3e0c3b5c74ceef4d9983928962ba6fd352d317
Kostya-Rozh <kosta7497@gmail.com>
2025-09-09 13:14:20 +0300

    removed js test-2

--- src/script.js
+++ src/script.js
@@ -2,7 +2,6 @@ console.log('hello');
 console.log('world');
 console.log('!');
 console.log('test');
-console.log('test2');
 
 window.onload = () => {
     alert("Здравствуйте, приветствуем вас на сайте");
